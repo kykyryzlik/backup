eb4e48a76d3ca7a17a8996ddbdc3d49dc768c86e
kykyryzlik <kykyryzlik@yandex.ru>
2020-02-27 16:53:56 +0300

    try1

--- backup.py
+++ backup.py
@@ -1,2 +1,23 @@
-print("HW")
-print("HW2")
+import os
+
+# def dirls(x):
+#     if x:
+#         a=os.listdir(x)
+#         #dirls(x[0)
+#     return(a)
+# 
+
+def dirls(x):
+    a=os.walk(x)
+    return(a)
+
+
+docdir="//home//voronkov//eclipse-workspace"
+dls=dirls(docdir)
+print(dls)
+
+for r, d, f in dls:
+    print(r)
+    print(d)
+    print(f)
+    print("---")
