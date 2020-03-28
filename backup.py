f68d52074fd1c20c6959f63d19df3bf71a3478b9
kyk <kyk@deb>
2020-03-28 19:50:40 +0300

    work blist

--- backup.py
+++ backup.py
@@ -1,80 +1,64 @@
 import os
+import configparser
 
-def dirls(pdir):
-# do list dir, need list of directories     
-    dlist=[]
-    flist=[]
-    if os.path.isdir(pdir):
-        ldir=os.listdir(pdir)
-        for i in ldir:
-            if os.path.isdir(os.path.join(pdir, i))==True:
-                dlist.append(i)
-            elif os.path.isdir(os.path.join(pdir, i))==False:
-                flist.append(i)
-    return(dlist, flist)
-    
+deep=1
+# if last simvol is / delete him
+srcdir="/home/kyk/wine/os/windows"
+srcdir=os.path.abspath(srcdir)
+srcdirdeep=srcdir.count(os.sep)
 
-def fpath(sdirs):
-# do full paths, need source path list AND list with directory`s and files names
-    fdir=[]
-    ffil=[]
-    for src in sdirs:
-        ld,lf=dirls(src)
-        for ndir in ld:
-            fdir.append(os.path.join(src, ndir))
-        for nfil in lf:
-            ffil.append(os.path.join(src, nfil))
-    return(fdir, ffil)
+def createConfig(pathConfig):
+    configparser.ConfigParser().add_section("Defaults")
+
+createConfig(os.getcwd())
 
 
-
-if os.name == 'nt':
-    srcdir=os.path.abspath("e:\dlna")
-    srcdirdeep=srcdir.count("\\")
-#     print(srcdirdeep)
-elif os.name == 'posix':
-    srcdir="~"
-
-deep=2
-# print("now deep is:", srcdirdeep, "\n", "need deep:", deep, "\n-----\n", adeep, "\n-----")
 
 
 def tree(blist):
     backuplist=[]
     for dir_path, dir_names, file_names in os.walk(srcdir):
-        if  dir_names == []:
-            dir_path_deep=dir_path.split(sep="\\")
-            print(dir_path_deep)
-            qwe=os.path.join(dir_path_deep[0], os.sep, dir_path_deep[1], dir_path_deep[2])
-            print(qwe)
-            backuplist.append(os.path.abspath(qwe))
-#dir_path.count("\\") < deep and
-#         elif dir_path.count("\\") < deep and dir_names != []:
-#             backuplist.append(os.path.abspath(dir_path))
-#         print(dir_path)
-    return backuplist
+        if dir_path.count(os.sep) == srcdirdeep+deep:
+            backuplist.append(dir_path)
+        elif  dir_names == [] and  dir_path.count(os.sep) <= srcdirdeep+deep:
+            backuplist.append(dir_path)
+    return(backuplist)
+
  
 b=tree(srcdir)
-print("\n-----", len(b))
 for i in b:
     print(i)
+
+print("--- len IS ", len(b))    
     
     
     
-###############
-# def countdown(num):
-#     print('Starting')
-#     while num > 0:
-#         yield num
-#         num -= 1
+    
+
+# def dirls(pdir):
+# # do list dir, need list of directories     
+#     dlist=[]
+#     flist=[]
+#     if os.path.isdir(pdir):
+#         ldir=os.listdir(pdir)
+#         for i in ldir:
+#             if os.path.isdir(os.path.join(pdir, i))==True:
+#                 dlist.append(i)
+#             elif os.path.isdir(os.path.join(pdir, i))==False:
+#                 flist.append(i)
+#     return(dlist, flist)
+#     
 # 
-# asd=countdown(10)
-# print(next(asd))
-# print(next(asd))
-# print("-------------")
-# for h in asd:
-#     print(h)
-
-
+# def fpath(sdirs):
+# # do full paths, need source path list AND list with directory`s and files names
+#     fdir=[]
+#     ffil=[]
+#     for src in sdirs:
+#         ld,lf=dirls(src)
+#         for ndir in ld:
+#             fdir.append(os.path.join(src, ndir))
+#         for nfil in lf:
+#             ffil.append(os.path.join(src, nfil))
+#     return(fdir, ffil)
 
 
